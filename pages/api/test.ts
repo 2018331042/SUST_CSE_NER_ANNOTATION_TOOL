6d9e5efb7334e8f1a7a476c853ec060aa393c96f
Fahim <fahim@pop-os.localdomain>
2023-03-14 14:29:24 +0600

    annotators payment data collected

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,34 +1,14 @@
 import connectDb from "../../lib/db";
 import Dataset from "../../lib/models/dataset";
-
+import User from "../../lib/models/user";
+import XLSX from "sheetjs-style";
+import * as FileSaver from "file-saver";
 export default async function handler(req, res) {
   await connectDb();
 
   try {
-    const response = await Dataset.aggregate([
-      {
-        $lookup: {
-          from: "users",
-          let: {
-            userid: "$user_id",
-          },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $eq: [
-                    {
-                      $toString: "$_id",
-                    },
-                    "$$userid",
-                  ],
-                },
-              },
-            },
-          ],
-          as: "user",
-        },
-      },
+    const users = await User.find({});
+    const dataset = await Dataset.aggregate([
       {
         $group: {
           _id: {
@@ -40,9 +20,27 @@ export default async function handler(req, res) {
         },
       },
     ]);
-    // console.log("hello");
 
-    res.json({ response });
+    let information = [];
+    users.map((user) => {
+      let totalTagwords = 0;
+      dataset.map((datam) => {
+        console.log({ words: user._id.toString() });
+        if (datam._id.user_id === user._id.toString()) {
+          if (
+            (datam._id.year === 2022 && datam._id.month >= 11) ||
+            (datam._id.year === 2023 && datam._id.month < 3)
+          )
+            totalTagwords = datam.total + totalTagwords;
+        }
+      });
+      information.push({
+        name: user.name,
+        total_words: totalTagwords,
+        Amount: Math.floor((500 * totalTagwords) / 3000),
+      });
+    });
+    res.json({ information });
   } catch (err) {
     console.log({ err });
     res.json({ err });
